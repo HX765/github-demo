bc2abc8e5991ac2f7256e750d9e3342d67acd6cf
Meredith Nachman <mwnachman@gmail.com>
2019-06-19 11:25:35 -0400

    made requested change

--- Git-practice.java
+++ Git-practice.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String args[]){
     System.out.println("This project is created by Han Xiao for the CIT591 Week 4 activity.\n"
     + "@Collaborator: please modify the next line, add, commit and push to Github.\n"
-    + "Please change here.\n"
+    + "CHANGED!!!!\n"
     + "Please DM me on slack once you are done. Thank you!");
   }
 
